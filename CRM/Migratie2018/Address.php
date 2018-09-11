d9172c91d152f6f23eb517a42347d6ce3c9e6f6e
ErikHommel <erik.hommel@civicoop.org>
2018-09-11 11:56:56 +0200

    haal nl woonplaats uit postcodetabel

--- CRM/Migratie2018/Address.php
+++ CRM/Migratie2018/Address.php
@@ -59,9 +59,7 @@ class CRM_Migratie2018_Address {
     if (!empty($streetAddress)) {
       $this->_addressData['street_address'] = implode(" ", $streetAddress);
     }
-    if (isset($sourceData['city'])) {
-      $this->_addressData['city'] = $sourceData['city'];
-    }
+    $this->_addressData['city'] = $this->getCity($sourceData);
     if (isset($sourceData['postal_code'])) {
       $this->_addressData['postal_code'] = $sourceData['postal_code'];
     }
@@ -76,6 +74,50 @@ class CRM_Migratie2018_Address {
       }
     }
     return TRUE;
+  }
+
+  /**
+   * Method om woonplaats te vullen. Als Nederland, haal uit postcode tabel als mogelijk
+   *
+   * @param $sourceData
+   * @return string $result
+   */
+  private function getCity($sourceData) {
+    $result = NULL;
+    if (isset($sourceData['city'])) {
+      $result = $sourceData['city'];
+      // als land Nederland, woonplaats op postcode zoeken
+      if ($sourceData['country_iso'] == "NL") {
+        if (isset($sourceData['postal_code']) && !empty($sourceData['postal_code'])) {
+          if (CRM_Core_DAO::checkTableExists('civicrm_postcodenl')) {
+            $postalCode = str_replace(' ', '', $sourceData['postal_code']);
+            if (strlen($postalCode) != 6) {
+              $this->_logger->logMessage('Waarschuwing', 'NL postcode ' . $postalCode
+                . ' onjuist geformatteerd, woonplaats uit bron gebruikt bij contact ID ' . $this->_contactId);
+            }
+            else {
+              $postCijfers = substr($postalCode,0,4);
+              $postLetters = substr($postalCode,4,2);
+              $query = "SELECT DISTINCT(woonplaats) FROM civicrm_postcodenl 
+                WHERE postcode_nr = %1 AND postcode_letter = %2";
+              $woonplaats = CRM_Core_DAO::singleValueQuery($query, [
+                1 => [$postCijfers, 'Integer'],
+                2 => [$postLetters, ' String'],
+              ]);
+              if (!empty($woonplaats)) {
+                $result = $woonplaats;
+              }
+              else {
+                $this->_logger->logMessage('Waarschuwing', 'Geen woonplaats gevonden voor postcode ' . $postalCode . ', woonplaats uit bron gebruikt bij contact ID ' . $this->_contactId);
+              }
+            }
+          }
+        } else {
+          $this->_logger->logMessage('Waarschuwing', 'Geen postcode voor adres in Nederland, woonplaats uit bron gebruikt bij contact ID ' . $this->_contactId);
+        }
+      }
+    }
+    return $result;
   }
 
   /**
