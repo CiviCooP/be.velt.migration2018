2e9eb7b4c1ef6c4c7536c8c045142612289f1e2d
ErikHommel <erik.hommel@civicoop.org>
2019-01-31 14:21:40 +0100

    fix adres per 1000

--- api/v3/Persoon/Fixaddress.php
+++ api/v3/Persoon/Fixaddress.php
@@ -14,12 +14,15 @@ function civicrm_api3_persoon_Fixaddress($params) {
   // haal de ids van het huishouden en hun adres op
   $queryHh = "SELECT hh.id AS huishouden_id, adr.id AS adres_id 
     FROM civicrm_contact AS hh JOIN civicrm_address AS adr ON hh.id = adr.contact_id AND adr.is_primary = %1
-    WHERE hh.contact_type = %2";
+    WHERE hh.contact_type = %2 AND hh.id NOT IN (SELECT hh_id FROM eh_tem_fixadr) LIMIT 1000";
   $hh = CRM_Core_DAO::executeQuery($queryHh, [
     1 => [1, 'Integer'],
     2 => ['Household', 'String'],
   ]);
   while ($hh->fetch()) {
+    // voeg hh_id toe aan tijdelijk bestand zodat die niet twee keer verwerkt wordt
+    $insert = "INSERT INTO eh_temp_fixadr (hh_id) VALUES(%1)";
+    CRM_Core_DAO::executeQuery($insert, [1 => [$hh->huishouden_id, 'Integer']]);
     // haal personen bij huishouden op, verwijder alle evt. adressen van persoon en geef shared address
     $queryPers = "SELECT contact_id_a AS persoon_id FROM civicrm_relationship
       WHERE relationship_type_id = %1 AND contact_id_b = %2";
