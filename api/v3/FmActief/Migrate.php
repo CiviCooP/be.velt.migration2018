005293ce6fbb07b2f274d6b8efa66f90417aa4a2
ErikHommel <erik.hommel@civicoop.org>
2019-07-13 09:48:11 +0200

    actief 200 per keer

--- api/v3/FmActief/Migrate.php
+++ api/v3/FmActief/Migrate.php
@@ -17,7 +17,7 @@ function civicrm_api3_fm_actief_Migrate($params) {
   $logCount = 0;
   $logger = new CRM_Migratie2018_Logger();
   $daoSource = CRM_Core_DAO::executeQuery('SELECT * FROM velt_migratie_2018.actief WHERE 
-    processed IS NULL OR processed = 0 LIMIT 250');
+    processed IS NULL OR processed = 0 LIMIT 200');
   while ($daoSource->fetch()) {
     $update = "UPDATE velt_migratie_2018.actief SET processed = %1 WHERE lidnummer = %2";
     CRM_Core_DAO::executeQuery($update, [
