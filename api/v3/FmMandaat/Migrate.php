472f5a89cc075bce94301e7e755c171a6d195bad
ErikHommel <erik.hommel@civicoop.org>
2019-07-13 11:45:38 +0200

    mandaten 200 per keer

--- api/v3/FmMandaat/Migrate.php
+++ api/v3/FmMandaat/Migrate.php
@@ -17,7 +17,7 @@ function civicrm_api3_fm_mandaat_Migrate($params) {
   $logCount = 0;
   $logger = new CRM_Migratie2018_Logger();
   $daoSource = CRM_Core_DAO::executeQuery('SELECT * FROM velt_migratie_2018.mandaat WHERE 
-    processed IS NULL OR processed = 0 LIMIT 250');
+    processed IS NULL OR processed = 0 LIMIT 200');
   while ($daoSource->fetch()) {
     $update = "UPDATE velt_migratie_2018.mandaat SET processed = %1 WHERE lidnummer = %2";
     CRM_Core_DAO::executeQuery($update, [
