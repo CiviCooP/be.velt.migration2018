896098186d722c04e19229965cbde694d4ffb985
ErikHommel <erik.hommel@civicoop.org>
2019-08-14 12:27:16 +0200

    ook testen op is_deleted

--- api/v3/VeltLid/Fixhoofd.php
+++ api/v3/VeltLid/Fixhoofd.php
@@ -14,7 +14,7 @@ function civicrm_api3_velt_lid_Fixhoofd($params) {
   // 14 aug 2019 haal alle onnodige hoofden van het huishouden op die geen lidmaatschap hebben (die staan er dubbel in)
   $query = "SELECT DISTINCT(a.id) AS contact_id FROM civicrm_contact AS a
 JOIN civicrm_relationship AS b ON a.id = b.contact_id_a AND b.relationship_type_id = %1
-WHERE a.contact_type = %2 AND a.id NOT IN(SELECT contact_id FROM civicrm_membership)";
+WHERE a.contact_type = %2 AND is_deleted != 1 AND a.id NOT IN(SELECT contact_id FROM civicrm_membership)";
   $dao = CRM_Core_DAO::executeQuery($query, [
     1 => [7, 'Integer'],
     2 => ['Individual', 'String'],
